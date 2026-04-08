6c8f33cf8ce95c9389d98561d320c4e8417126e2
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 00:39:48 +0530

    Update Docker_Login.groovy

--- vars/Docker_Login.groovy
+++ vars/Docker_Login.groovy
@@ -1,12 +1,18 @@
-def call(String credId){
-withCredentials([usernamePassword(
-    credentialsId: credId,
-    usernameVariable: 'DOCKER_USER',
-    passwordVariable: 'DOCKER_PASS'
-)]) {
+def call(String credId) {
+    def username = ""
 
-    sh '''
-    echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
-    '''
-}
+    withCredentials([usernamePassword(
+        credentialsId: credId,
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+
+        sh '''
+        echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+        '''
+
+        username = env.DOCKER_USER
+    }
+
+    return username
 }
