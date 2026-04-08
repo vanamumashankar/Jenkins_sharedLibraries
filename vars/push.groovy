ac0ac739cdcca0dbcbf5d32dca0e850b679a6da6
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 00:43:02 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,7 +1,7 @@
 def call(String credId, String imageName, String tagname){
 def user = Docker_Login(credId)
                 sh '''
-                docker push ${env.dockerHubUser}/${imageName}:${tagname}
+                docker push ${user}/${imageName}:${tagname}
                 '''
                 }  
 }
