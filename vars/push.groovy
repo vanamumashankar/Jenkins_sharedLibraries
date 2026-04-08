3f6ca0e440f2f467f705569dc75b886838f7a9cc
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 01:04:32 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -6,6 +6,6 @@ def call(String credId, String imageName, String tagname) {
     docker tag ${user}/${imageName} ${user}/${imageName}:${tagname}
     docker push ${user}/${imageName}:${tagname}
     docker compose down
-    docker run --name notes -p 8000:8000 ${user}/${imageName}:${tagname}
+    docker run -d --name notes -p 8000:8000 ${user}/${imageName}:${tagname}
     """
 }
