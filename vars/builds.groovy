7009bf7478898ea9238b4c848d8aaf64c4a8c23a
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 00:37:23 +0530

    Update builds.groovy

--- vars/builds.groovy
+++ vars/builds.groovy
@@ -1,6 +1,6 @@
 def call(String credId, String imageName){
 Docker_Login(credId)
 sh """
-  docker build -t ${env.dockerHubUser}/${imageName} .
+  docker build -t "${env.dockerHubUser}"/"${imageName}" .
   """
 }
