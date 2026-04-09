fd3c76937dc3fd344071016d808a5610adfcd00f
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 21:42:05 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -9,16 +9,5 @@ def call(String credId, String imageName, String tagname) {
     # Push image
     docker push ${user}/${imageName}:${tagname}
 
-    # Stop and remove old container if exists
-    docker rm -f notes || true
-
-    # Run new container
-  #  docker run -d --name notes -p 8000:8000 ${user}/${imageName}:${tagname}
-    docker run -d --name notes -p 8000:8000 \
--e DB_NAME=notes \
--e DB_USER=root \
--e DB_PASSWORD=password \
--e DB_HOST=mysql \
-${user}/${imageName}:${tagname}
-    """
+docker compose down && docker compose up
 }
