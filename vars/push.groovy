3ae3dd7f3a660258d4cc00f7813e6e0f97b0d63b
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 21:05:05 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -3,9 +3,16 @@ def call(String credId, String imageName, String tagname) {
     def user = Docker_Login(credId)
 
     sh """
+    # Tag image (only if needed)
     docker tag ${user}/${imageName} ${user}/${imageName}:${tagname}
+
+    # Push image
     docker push ${user}/${imageName}:${tagname}
-    docker compose down
+
+    # Stop and remove old container if exists
+    docker rm -f notes || true
+
+    # Run new container
     docker run -d --name notes -p 8000:8000 ${user}/${imageName}:${tagname}
     """
 }
