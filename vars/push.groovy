69a8401925732a58d32755bccd22f95da8eba985
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-09 21:35:37 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -13,6 +13,12 @@ def call(String credId, String imageName, String tagname) {
     docker rm -f notes || true
 
     # Run new container
-    docker run -d --name notes -p 8000:8000 ${user}/${imageName}:${tagname}
+  #  docker run -d --name notes -p 8000:8000 ${user}/${imageName}:${tagname}
+    docker run -d --name notes -p 8000:8000 \
+-e DB_NAME=notes \
+-e DB_USER=root \
+-e DB_PASSWORD=password \
+-e DB_HOST=mysql \
+${user}/${imageName}:${tagname}
     """
 }
