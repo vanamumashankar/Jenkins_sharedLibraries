78c8dea86490862b69b04c22ac17003a4d505e65
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-10 20:31:24 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -3,12 +3,40 @@ def call(String credId, String imageName, String tagname) {
     def user = Docker_Login(credId)
 
     sh """
-    # Tag image (only if needed)
+    # Tag & Push
     docker tag ${user}/${imageName} ${user}/${imageName}:${tagname}
-
-    # Push image
     docker push ${user}/${imageName}:${tagname}
 
-docker compose down && docker compose up
-"""
+    # Create network (ignore if exists)
+    docker network create my || true
+
+    # Create volume
+    docker volume create mysql_data || true
+
+    # Remove old containers if exist
+    docker rm -f mysql notes || true
+
+    # Run MySQL container
+    docker run -d \
+      --name mysql \
+      --network my \
+      -e MYSQL_ROOT_PASSWORD=root \
+      -e MYSQL_DATABASE=test_db \
+      -v mysql_data:/var/lib/mysql \
+      mysql:5.7
+
+    # Wait for MySQL to start
+    sleep 20
+
+    # Run Django container
+    docker run -d \
+      --name notes \
+      --network my \
+      -p 8000:8000 \
+      -e DB_NAME=test_db \
+      -e DB_USER=root \
+      -e DB_PASSWORD=root \
+      -e DB_HOST=mysql \
+      ${user}/${imageName}:${tagname}
+    """
 }
