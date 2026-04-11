6ab829d93127b130adc1efb8ea518efa781eeb51
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-11 20:05:31 +0530

    Create trivy.groovy

---
+++ vars/trivy.groovy
@@ -0,0 +1,8 @@
+def call(String filename){
+  sh """
+  echo "Trivy Scaning started from here"
+  echo "====================================================="
+  trivy fs --format table -o ${filename}-report.html .
+  echo "====================================================="
+  echo "trvivy scnanned docker image and report has been generated" 
+}
