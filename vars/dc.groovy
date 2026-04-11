f1d9808304886c6bf50cccc5eab0212278de8034
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-11 20:15:57 +0530

    Update dc.groovy

--- vars/dc.groovy
+++ vars/dc.groovy
@@ -1,6 +1,10 @@
 def call(String filename, String toolname){
   sh """
+  echo "dc check ha been started"
+  echo "==============================="
   dependencyCheck additionalArguments: '--scan ./', odcInstallation: '${toolname}'
 dependencyCheckPublisher pattern: '**/${filename}-report.xml'
+echo "======================================="
+echo "dc checked"
 """
 }
