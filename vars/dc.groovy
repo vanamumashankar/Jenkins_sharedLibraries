ef9755b95b49be30e485bbd3fc625061ec156136
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-11 20:35:00 +0530

    Update dc.groovy

--- vars/dc.groovy
+++ vars/dc.groovy
@@ -3,9 +3,9 @@ def call(String filename, String toolname){
     echo "Dependency Check started"
     echo "==============================="
 
-    dependencyCheck additionalArguments: "--scan ./ --format XML --out .", odcInstallation: toolname
+    dependencyCheck additionalArguments: "--scan ./ --format XML --out .", odcInstallation: ${toolname}
 
-    dependencyCheckPublisher pattern: "**/dependency-check-report.xml"
+    dependencyCheckPublisher pattern: "**/${filename}-report.xml"
 
     echo "======================================="
     echo "Dependency Check completed"
