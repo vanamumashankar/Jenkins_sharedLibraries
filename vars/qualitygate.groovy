cfc2a305ce7d39885260e62dd1a6cc7381da13a2
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-11 20:12:59 +0530

    Update qualitygate.groovy

--- vars/qualitygate.groovy
+++ vars/qualitygate.groovy
@@ -1,5 +1,11 @@
-def cal(){
+def call(){
+  sh """
+  echo "sonar qube quality gates scanning"
+  echo "===================================="
   timeout(time: 2, unit: "MINUTES"){
 waitForQualityGate abortPipeline: false
   }
+  echo "===================================="
+  echo "sonarqube quality gates check completed"
+  """
 }
