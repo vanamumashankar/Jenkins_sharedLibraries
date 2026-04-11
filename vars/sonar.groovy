6c095645cd52b32f8067385859a46f88527b6041
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-04-11 21:55:40 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,12 +1,15 @@
 def call(String toolname, String name){
-  sh """
-  echo "sonar project created"
-  echo "======================================"
-withSonarQubeEnv("${toolname}"){
-$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${name} -Dsonar.projectKey=${name}
-echo "======================================"
-echo "Sonar qube ended"
-}
-"""
-}
+    echo "sonar project created"
+    echo "======================================"
 
+    withSonarQubeEnv("${toolname}") {
+        sh """
+        ${SONAR_HOME}/bin/sonar-scanner \
+        -Dsonar.projectName=${name} \
+        -Dsonar.projectKey=${name}
+        """
+    }
+
+    echo "======================================"
+    echo "Sonar qube ended"
+}
